b54d81dc64a79bcc939f41d10aac64d176b78bb9
Arifudin <143675110+Arifester@users.noreply.github.com>
2025-05-18 22:55:46 +0700

    docs: add comments to WelcomeController.php

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,8 +6,10 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
+    // Fungsi index untuk menampilkan halaman welcome-again
     public function index()
     {
+         // Mengembalikan view 'welcome-again'
          return view(view: 'welcome-again');
     }
 
